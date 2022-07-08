c4975f2d6c46a8fe9e9e3d6ef8b07f6f8db7cde7
DESKTOP-OPSHN91\DIGIPARC <abdelouahed-maroc@live.fr>
2022-07-08 17:16:02 +0100

    Name changing

--- task-manager-front/src/app/app.component.spec.ts
+++ task-manager-front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-task-front'`, () => {
+  it(`should have as title 'task-manager-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('crud-task-front');
+    expect(app.title).toEqual('task-manager-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('crud-task-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('task-manager-front app is running!');
   });
 });
