23e6c981a360bfa727fafff1919bbddc8774b247
LAPTOP-0UQHH1BP\abdel <abdelouahed-maroc@live.fr>
2022-07-18 01:44:49 +0100

    Finishing UI and Starting working on connection with backend

--- task-manager-front/src/app/app.module.ts
+++ task-manager-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskScreenComponent } from './screen/task-screen/task-screen.component';
@@ -12,7 +13,8 @@ import { TaskScreenComponent } from './screen/task-screen/task-screen.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- task-manager-front/src/app/screen/task-screen/task-screen.component.ts
+++ task-manager-front/src/app/screen/task-screen/task-screen.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskScreenComponent implements OnInit {
 
+  taskGroups : any[] = [];
+  tasks : any[] = [];
   constructor() { }
 
   ngOnInit() {
